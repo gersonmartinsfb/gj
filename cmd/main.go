6422d95c6e64a7278588aede4ffd5677aa9748d0
Gerson Luiz Barizon Martins <gerson.martins@foxbit.com.br>
2025-05-29 11:00:47 -0300

    Add more help info

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,13 @@ func main() {
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "  type: The type of issue (bug, feat, hotfix, enhance)\n")
 		fmt.Fprintf(os.Stderr, "  issue-number: The issue number to create a branch for. Just the number. The suffix %s- will be added automatically.\n", config.Env.JiraIssuePrefix)
+
+		fmt.Fprintf(os.Stderr, "\nExtra:\n")
+		fmt.Fprintf(os.Stderr, "    feat -- Para feature\n")
+		fmt.Fprintf(os.Stderr, "    hotfix -- Problema que precisa entrar como prioridade em prod ASAP\n")
+		fmt.Fprintf(os.Stderr, "    bug -- Resolução de bug\n")
+		fmt.Fprintf(os.Stderr, "    enhance -- Refactor / melhoria / otimização / lynt / resolução de ofensas\n")
+
 	}
 
 	flag.Parse()
