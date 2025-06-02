27471e58787b23879a8e3b9d6ad61690bd593839
Gerson Luiz Barizon Martins <gerson.martins@foxbit.com.br>
2025-06-02 17:08:11 -0300

    Add aliases to type

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: gj [--disable-creation] <type> <issue-number>\n")
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		flag.PrintDefaults()
-		fmt.Fprintf(os.Stderr, "  type: The type of issue (bug, feat, hotfix, enhance)\n")
+		fmt.Fprintf(os.Stderr, "  type: The type of issue (bug, feat, hotfix, enhance). Could be the initial letter (b, f, h, e)\n")
 		fmt.Fprintf(os.Stderr, "  issue-number: The issue number to create a branch for. Just the number. The suffix %s- will be added automatically.\n", config.Env.JiraIssuePrefix)
 
 		fmt.Fprintf(os.Stderr, "\nExtra:\n")
@@ -40,10 +40,31 @@ func main() {
 	issueNumber := args[1]
 	config.Env.CreateBranch = !(*disableBranch)
 
+	argType = returnFullName(argType)
+
 	branch := createbranch.NewCreateBranch()
 	err := branch.CreateBranch(argType, issueNumber)
 	if err != nil {
 		fmt.Printf("Error creating branch for issue %s: %v\n", issueNumber, err)
 		os.Exit(1)
 	}
+}
+
+func returnFullName(argType string) string {
+	switch argType {
+	case "b", "bug":
+		return "bug"
+	case "f", "feat":
+		return "feat"
+	case "h", "hotfix":
+		return "hotfix"
+	case "e", "enhance":
+		return "enhance"
+	default:
+		fmt.Printf("Unknown type: %s\n", argType)
+		fmt.Println("Valid types are: b/bug, f/feat, h/hotfix, e/enhance")
+		os.Exit(1)
+	}
+
+	return ""
 }
